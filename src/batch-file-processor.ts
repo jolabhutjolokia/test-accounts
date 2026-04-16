17d72e09cb5f42580bcf3168a909c1a5d96bd690
piggy <piggy>
2026-04-16 15:30:20 +1000

    pull all batch processing logic into one place

---
+++ src/batch-file-processor.ts
@@ -0,0 +1,56 @@
+import { BankAccountService } from './services/bank-account-service';
+import { parseBalancesFile, ParsedBalance } from './input-parsers/csv/balance-csv-parser';
+import { ParsedTransaction, parseTransactionsFile } from './input-parsers/csv/transactions-csv-parser';
+import { Money } from './models/money';
+import { FailureDetails } from './models/failures';
+
+function createInitialBalances(balances: ParsedBalance[], service: BankAccountService) {
+  return balances.reduce<FailureDetails[]>((acc, balance) => {
+    const amountResult = Money.create(balance.amount, "AUD");
+    if (amountResult.status === 'failure') {
+      acc.push(amountResult.details)
+      return acc;
+    }
+
+    const createResult = service.createAccount(balance.accountId, amountResult.data);
+    if (createResult.status === 'success') return acc;
+
+    acc.push(createResult.details)
+    return acc;
+  }, []);
+}
+
+function processAllTransactions(transactions: ParsedTransaction[], service: BankAccountService) {
+  return transactions.reduce<FailureDetails[]>((acc, transaction) => {
+    const amountResult = Money.create(transaction.amount, "AUD");
+    if (amountResult.status === 'failure') {
+      acc.push(amountResult.details)
+      return acc;
+    }
+
+    const transferResult = service.transfer(transaction.fromId, transaction.toId, amountResult.data);
+    if (transferResult.status === 'failure') {
+      acc.push(transferResult.details);
+    }
+    return acc;
+  }, [])
+}
+
+export const createBatchFileProcessor = (service: BankAccountService) => (args: {
+  balanceFile: string;
+  transactionsFile: string;
+}) => {
+  const balances = parseBalancesFile(args.balanceFile);
+  const transactions = parseTransactionsFile(args.transactionsFile);
+  const balanceCreationFailures = createInitialBalances(balances.successes, service);
+  const transactionProcessingFailures = processAllTransactions(transactions.successes, service);
+
+  return {
+    failures: {
+      balanceParsingFailures: balances.failures,
+      transactionParsingFailures: transactions.failures,
+      balanceCreationFailures,
+      transactionProcessingFailures
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,38 @@
 import { BankAccountService } from './services/bank-account-service';
 import { InMemoryBankAccountRepository } from './repositories/in-memory-bank-account-repository';
-import { parseBalancesFile } from './input-parsers/csv/balance-csv-parser';
-import { parseTransactionsFile } from './input-parsers/csv/transactions-csv-parser';
-import { Money } from './models/money';
+import { createBatchFileProcessor } from './batch-file-processor';
 
-const balances = parseBalancesFile("./test-data/mable_account_balances.csv");
-const transactions = parseTransactionsFile('./test-data/mable_transactions.csv');
 const repository = new InMemoryBankAccountRepository();
 const service = new BankAccountService(repository);
+const batchFileProcessor = createBatchFileProcessor(service);
+
+const {
+  failures: {
+    transactionParsingFailures,
+    transactionProcessingFailures,
+    balanceParsingFailures,
+    balanceCreationFailures
+  }
+} = batchFileProcessor({
+  balanceFile: "./test-data/mable_account_balances.csv",
+  transactionsFile: './test-data/mable_transactions.csv'
+});
 
 console.log("-----------Start Parsing Failures ------------------");
-balances.failures.forEach((x) => {
+balanceParsingFailures.forEach((x) => {
   console.error("Balance parse failure", x);
 })
-transactions.failures.forEach((x) => {
+transactionParsingFailures.forEach((x) => {
   console.error("Transaction parse failure", x);
+})
+balanceCreationFailures.forEach((x) => {
+  console.error("Failed to create balance", x);
+})
+transactionProcessingFailures.forEach((x) => {
+  console.error("Failed to create balance", x);
 })
 console.log("-----------End Parsing Failures ------------------");
 
-
-balances.successes.forEach((balance) => {
-  const amountResult = Money.create(balance.amount, "AUD");
-  if (amountResult.status === 'failure') {
-    console.error("Balance amount is invalid", amountResult.details);
-    return;
-  }
-  const createResult = service.createAccount(balance.accountId, amountResult.data);
-  if (createResult.status === 'failure') {
-    console.error("Could not create account", createResult.details)
-  }
-});
-
-transactions.successes.forEach((transaction) => {
-  const amountResult = Money.create(transaction.amount, "AUD");
-  if (amountResult.status === 'failure') {
-    console.error("Transfer amount is invalid", amountResult.details);
-    return;
-  }
-  const transferResult = service.transfer(transaction.fromId, transaction.toId, amountResult.data);
-  if (transferResult.status === 'failure') {
-    console.error("Failed to transfer", transferResult.details)
-  }
-})
 
 console.log("---------- all account balances ---------");
 
